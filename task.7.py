4d027872750ac9f6bfc2bca212091b5cf02c5aed
Sonyabyko <166370985+Sonyabyko@users.noreply.github.com>
2024-05-30 16:30:15 +0300

    я массивы сделала в таск семь неачаяно

--- task.7.py
+++ task.7.py
@@ -37,4 +37,43 @@ if has_order and (is_first_order or total_purchase > min_purchase):
 # if number = [7, 13, 21]
 # print("счастливое число")
 
+k, l = [], []
+for i in range(1, 11):
+    k.appened(10 -i)
+for i in range(1, 11):
+    l.appened(k[5] -k[i -1])
+print(k)
+print(l)
 
+k, l = list(range(1, 11)), list(range(1, 11))
+
+for i in (range(1, 11)):
+    k[i - 1]  = 10 - i
+for i in (range(1, 11)):
+    l[i - 1] = k[5] - k[i -1]
+print(k)
+print(l)
+print(f'Кошличество отрицательных элементов массива l = len([value for value in l ifvalue < 0])}')
+
+k, l = list(range(1, 11)), list(range(1, 11))
+
+for i in (range(1, 11)):
+    k[i - 1] = 10 - i
+for i in (range(1, 11)):
+    l[i - 1] = k[5] - k[i - 1]
+print(k)
+print(l)
+count_k = 0
+for value in k:
+        if value < 0:
+            count_k += 1
+count_l = 0
+for value in l:
+    if value < 0:
+        count_l += 1
+print(f'Количество отрицательных элементов = count_k + count_l}')
+
+#List comprehensions
+count_k = len([value for value in k if value < 0])
+count_l = len([value for value in l if value < 0])
+print(f'Количество отрицательных элементов = count_k + count_l}')
